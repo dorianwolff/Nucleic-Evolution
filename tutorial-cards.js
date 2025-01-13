e4ed888e8374266c24028076f99df37bc6cdf865
dorian.wolff <dorian.wolff@epita.fr>
2025-01-13 16:10:33 +0100

    feature: working on the tutorial mode, added new cards for this. Added more elements to the profile page

---
+++ tutorial-cards.js
@@ -0,0 +1,28 @@
+// Effect structure : timing, zones affected, condition/requirements, parameter, typeAffected, description
+const cards = [
+    { id: 1, name: 'Unit Card', type: 'unit', faction: ['None'], resource: 8, combat: 0,rawResource: 2, rawCombat: 6, placement: 1, status: [], cardName: 'Nocturn',
+        effect: [['None']],
+        image: 'Individual_Cards/Tutorial/fronts/0.png' },
+
+    { id: 2, name: 'Unit Card', type: 'unit', faction: ['None'], resource: 8, combat: 0,rawResource: 5, rawCombat: 3, placement: 1, status: [], cardName: 'Slyde',
+        effect: [['None']],
+        image: 'Individual_Cards/Tutorial/fronts/1.png' },
+
+    { id: 3, name: 'Unit Card', type: 'unit', faction: ['None'], resource: 8, combat: 0,rawResource: 4, rawCombat: 4, placement: 1, status: [], cardName: 'Shogun',
+        effect: [['None']],
+        image: 'Individual_Cards/Tutorial/fronts/2.png' },
+
+    { id: 4, name: 'Unit Card', type: 'unit', faction: ['None'], resource: 8, combat: 0,rawResource: 6, rawCombat: 2, placement: 1, status: [], cardName: 'Paladin',
+        effect: [['None']],
+        image: 'Individual_Cards/Tutorial/fronts/3.png' },
+
+    { id: 5, name: 'Unit Card', type: 'unit', faction: ['None'], resource: 8, combat: 0,rawResource: 5, rawCombat: 3, placement: 1, status: [], cardName: 'Trainee',
+        effect: [['None']],
+        image: 'Individual_Cards/Tutorial/fronts/4.png' },
+
+    { id: 6, name: 'Unit Card', type: 'unit', faction: ['None'], resource: 8, combat: 0,rawResource: 3, rawCombat: 5, placement: 1, status: [], cardName: 'Apprentice',
+        effect: [['None']],
+        image: 'Individual_Cards/Tutorial/fronts/5.png' },
+];
+
+window.cards = cards;
