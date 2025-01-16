1aac4a6a5841de4612cb5230d3b425b116f4059c
dorian.wolff <dorian.wolff@epita.fr>
2025-01-17 00:15:52 +0100

    fix: added missing AI card and implemented them correctly

--- tutorial-config.js
+++ tutorial-config.js
@@ -66,7 +66,7 @@ const stages = [
         x: 3200, y: 400, 
         label: "Final Challenge",
         rewards: {
-            cards: [7, 21, 22, 14, 16],
+            cards: [7, 23, 22, 14, 16],
             coins: 50
         }
     }
