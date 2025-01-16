a5a455f9d756724e1839acd3c79c743e65c0919b
dorian.wolff <dorian.wolff@epita.fr>
2025-01-17 00:13:47 +0100

    fix: added missing AI card and implemented them correctly

--- tutorial-cards.js
+++ tutorial-cards.js
@@ -68,29 +68,33 @@ const tutorialCards = [
         effect: [['None']],
         image: 'Individual_Cards/Tutorial/fronts/16.png' },
 
-    { id: 17, name: 'Power-Up Card', type: 'power up', faction: ['basic'], resource: 0, combat: 0,rawResource: 0, rawCombat: 0, placement: 1, status: [], cardName: 'Wanderer\'s Route',
+    { id: 17, name: 'Power-Up Card', type: 'power up', faction: ['basic'], resource: 0, combat: 0,rawResource: 0, rawCombat: 0, placement: 1, status: [], cardName: 'Powerful Stick',
         effect: [['None']],
         image: 'Individual_Cards/Tutorial/fronts/17.png' },
 
-    { id: 18, name: 'Power-Up Card', type: 'power up', faction: ['basic'], resource: 0, combat: 0,rawResource: 0, rawCombat: 0, placement: 1, status: [], cardName: 'Wanderer\'s Route',
+    { id: 18, name: 'Power-Up Card', type: 'power up', faction: ['basic'], resource: 0, combat: 0,rawResource: 0, rawCombat: 0, placement: 1, status: [], cardName: 'Average Sword',
         effect: [['None']],
         image: 'Individual_Cards/Tutorial/fronts/18.png' },
 
-    { id: 19, name: 'Field', type: 'civilization', faction: ['basic'], resource: 0, combat: 0,rawResource: 0, rawCombat: 0, placement: 1, status: [], cardName: 'Wanderer\'s Route',
+    { id: 19, name: 'Field', type: 'civilization', faction: ['basic'], resource: 0, combat: 0,rawResource: 0, rawCombat: 0, placement: 1, status: [], cardName: 'A New Dawn',
         effect: [['None']],
         image: 'Individual_Cards/Tutorial/fronts/19.png' },
 
-    { id: 20, name: 'Civilization Card', type: 'civilization', faction: ['basic'], resource: 0, combat: 0,rawResource: 0, rawCombat: 0, placement: 1, status: [], cardName: 'Wanderer\'s Route',
+    { id: 20, name: 'Civilization Card', type: 'civilization', faction: ['basic'], resource: 0, combat: 0,rawResource: 0, rawCombat: 0, placement: 1, status: [], cardName: 'War Pluder',
         effect: [['None']],
         image: 'Individual_Cards/Tutorial/fronts/20.png' },
 
-    { id: 21, name: 'Unit Card', type: 'unit', faction: ['basic'], resource: 2, combat: 2,rawResource: 2, rawCombat: 2, placement: 1, status: [], cardName: 'Wanderer\'s Route',
+    { id: 21, name: 'Land Card', type: 'land', faction: ['basic'], resource: 10, combat: 0,rawResource: 10, rawCombat: 0, placement: 1, status: [], cardName: 'Forsaken Village',
         effect: [['None']],
         image: 'Individual_Cards/Tutorial/fronts/21.png' },
 
-    { id: 22, name: 'Unit Card', type: 'unit', faction: ['basic'], resource: 4, combat: 1,rawResource: 4, rawCombat: 1, placement: 1, status: [], cardName: 'Wanderer\'s Route',
+    { id: 22, name: 'Unit Card', type: 'unit', faction: ['basic'], resource: 4, combat: 1,rawResource: 4, rawCombat: 1, placement: 1, status: [], cardName: 'Fierce Retainer',
         effect: [['None']],
         image: 'Individual_Cards/Tutorial/fronts/22.png' },
+    
+    { id: 23, name: 'Unit Card', type: 'unit', faction: ['basic'], resource: 2, combat: 2,rawResource: 2, rawCombat: 2, placement: 1, status: [], cardName: 'Skeleton Soldier',
+        effect: [['None']],
+        image: 'Individual_Cards/Tutorial/fronts/23.png' },
 ];
 
 window.tutorialCards = tutorialCards;
